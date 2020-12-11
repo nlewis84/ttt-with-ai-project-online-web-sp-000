7736c12e85f63502e42387dfcbb44133881dd370
Nathan Lewis <n.lewis.perc@gmail.com>
2020-12-11 23:12:47 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,12 @@
 module Players
   class Computer < Player
     def move(board)
-
+        binding.pry
+        board.cells.each_with_index do |cell, index|
+          if cell == " "
+            return index
+          end
+        end
     end
   end
 end
