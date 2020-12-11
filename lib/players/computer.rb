b3d2c90df6f56280cf1e5c58693deef2fbf913dc
Nathan Lewis <n.lewis.perc@gmail.com>
2020-12-11 23:21:10 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,11 @@
 module Players
   class Computer < Player
     def move(board)
-
+        board.cells.each_with_index do |cell, index|
+          if cell == " "
+            return (index + 1).to_s
+          end
+        end
     end
   end
 end
